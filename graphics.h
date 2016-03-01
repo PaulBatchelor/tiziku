c01983a024644f9ee4ce85a52e5c956d57c7e0e0
Paul Batchelor <thisispaulbatchelor@gmail.com>
2016-02-29 23:14:45 -0800

    added missing files

---
+++ graphics.h
@@ -0,0 +1,16 @@
+#ifndef TZ_GRAPHICS
+#define TZ_GRAPHICS
+
+#include <GLFW/glfw3.h>
+#include "nanovg.h"
+
+typedef struct {
+    GLFWwindow *window;
+    NVGcontext *vg;
+    float g_time;
+} tz_graphics;
+
+void tz_run_graphics(tz_graphics *gfx, void (*draw)(NVGcontext*,GLFWwindow*,void*), void *ud);
+void tz_stop_graphics(tz_graphics *gfx);
+
+#endif
